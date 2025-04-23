2134f86d61201ebee0c67134063c8f8dc2a78297
Suvid Thungathurti <suvidthungathurti@Suvids-MacBook-Air.local>
2025-04-23 13:10:20 -0400

    finished the add_task method

--- main.cpp
+++ main.cpp
@@ -30,9 +30,9 @@ public:
         this->next = nullptr;
     }
 
-    ~Task() {
-        delete next;
-    };
+    int get_id() const {
+        return this->id;
+    }
 
     string get_description() const {
         return this->description;
@@ -63,28 +63,46 @@ public:
 class Tasks {
 private:
     Task* head;
+    int len;
 
 public:
     Tasks() {
         this->head = nullptr;
+        this->len = 0;
     };
 
     // not sure how this works exactly, or why there aren't other ways of doing this, will have to ask later
     ~Tasks() {
         Task* current = head;
-
         while (current != nullptr) {
             Task* next = current->get_next();
             delete current;
             current = next;
         }
+
     };
 
     int add_task(const string& description) {
-        return 0;
+        len += 1;
+        Task* newTask = new Task(len, description);
+
+        newTask->set_next(head);
+        head = newTask;
+        
+        return head->get_id();
     };
 
-    void update_task(int id, const string& newDescrioption);
+    void update_task(int id, const string& newDescrioption) {
+        if (head == nullptr) {
+            throw invalid_argument("There are not tasks to be updated");
+        }
+        
+        Task* current = head;
+
+        while (current != nullptr) {
+
+        }
+    };
 
     void delete_task(int id);
 
@@ -96,5 +114,92 @@ public:
 // -------------------------
 
 int main() {
+    while (true) {
+        Tasks taskManager{};
+        string command;
+
+        cout << "TaskMaanger ";
+        cin >> command;
+
+        if (command == "add") {
+            string description;
+
+            cin >> description;
+
+            taskManager.add_task(description);
+
+        } else if (command == "update") {
+            int id;
+            string description;
+
+            cin >> id >> description;
+
+            taskManager.update_task(id, description);
+
+        } else if (command == "delete") {
+            int id;
+
+            cin >> id;
+
+            taskManager.delete_task(id);
+
+        } else if (command == "mark") {
+            string command2;
+
+            cin >> command2;
+
+            if (command2 == "todo") {
+                int id;
+
+                cin >> id;
+
+                taskManager.change_progress(id, Task::Progress::Todo);
+
+            } else if (command2 == "in-progress") {
+                int id;
+
+                cin >> id;
+
+                taskManager.change_progress(id, Task::Progress::InProgress);
+
+            } else if (command2 == "done") {
+                int id;
+
+                cin >> id;
+
+                taskManager.change_progress(id, Task::Progress::Done);
+
+            } else {
+                cout << "Command Not Understood, Try Again";
+
+            }
+
+        } else if (command == "list") {
+            string command2;
+
+            cin >> command2;
+
+            if (command2 == "all") {
+                taskManager.list_tasks(Task::Progress::None);
+
+            } else if (command2 == "todo") {
+                taskManager.list_tasks(Task::Progress::Todo);
+
+            } else if (command2 == "in-progress") {
+                taskManager.list_tasks(Task::Progress::InProgress);
+
+            } else if (command2 == "done") {
+                taskManager.list_tasks(Task::Progress::Done);
+
+            } else {
+                cout << "Command Not Understood, Try Again";
+
+            }
+        } else {
+            cout << "Command Not Understood, Try Again";
+
+        }
+
+    }
 
 }
